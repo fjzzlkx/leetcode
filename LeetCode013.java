b96f02641ed54636b7497f0df6fcce16f6056e13
fjzzlkx <fjzzlkx@163.com>
2016-12-13 20:45:16 +0800

    Create LeetCode013.java

---
+++ LeetCode013.java
@@ -0,0 +1,37 @@
+package research;
+import java.util.Map;
+import java.util.HashMap;
+public class Test {
+	public static void main(String[] args){
+		Solution s = new Solution();
+		String sss = "MMMDLXXXVI";
+		int i = s.romanToInt(sss);
+		System.out.println(i);
+	}
+}
+
+class Solution {
+    public int romanToInt(String s) {
+    	Map<Character, Integer> romanMap = new HashMap<>();
+    	romanMap.put('I', 1);
+    	romanMap.put('V', 5);
+    	romanMap.put('X', 10);
+    	romanMap.put('L', 50);
+    	romanMap.put('C', 100);
+    	romanMap.put('D', 500);
+    	romanMap.put('M', 1000);
+    	
+    	int value = 0;
+    	int i = 0;
+        for(i = 0; i < s.length(); i++){
+        	if(i+1 < s.length() && romanMap.get(s.charAt(i)) < romanMap.get(s.charAt(i + 1))){
+        		value = value + romanMap.get(s.charAt(i + 1)) - romanMap.get(s.charAt(i));
+        		i++;
+        	} else {
+        		value = value + romanMap.get(s.charAt(i));
+        	}
+     
+        }
+        return value;
+    }
+}
